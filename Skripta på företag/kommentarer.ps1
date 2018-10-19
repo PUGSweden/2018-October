0b63147e6dd6f86f24bcbbd366c1fecfec46f1b8
Fredrik Wall <fredrik@poweradmin.se>
2018-10-19 10:01:19 +0200

    Agenda, and code from Fredriks sessions

---
+++ Skripta på företag/kommentarer.ps1
@@ -0,0 +1,37 @@
+<#
+    .SYNOPSIS
+        En kort beskrivning om funktionen eller skriptet.    
+    
+    .DESCRIPTION
+        En längre beskrivning om funktionen eller skriptet
+
+    .PARAMETER FirstParameter
+        En beskrivning per parameter
+
+    .PARAMETER SecondParameter
+        En beskrivning per parameter
+
+    .INPUTS
+        Beskrivning av objekt som kan pipas till skriptet    
+    
+    .OUTPUTS
+        Beskrivning av objekt som skickas som output av skriptet    
+    
+    .EXAMPLE
+        Exempel på hur man kör funktionen eller skriptet
+
+    .LINK
+        Länk till mer information om funktionen eller skriptet
+
+    .NOTES
+        Mer information om skriptet.
+        T ex:
+        Version:        Versionsnummer
+        Author:         Förnamn Efternamn, mailadress
+        Creation Date:  Datum
+        Last change:    Datum
+		Dependencies:   Kräver bla bla
+#>
+
+# Mer information om comment-cased help kan man hitta här:
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-6
